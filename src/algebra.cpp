f4c903c3ba86fd0a695382157aa6e27ed9757a74
Dmitry <evtushenko.2002@mail.ru>
2019-02-02 19:39:18 +0300

    Auto solve finished

--- src/algebra.cpp
+++ src/algebra.cpp
@@ -18,48 +18,52 @@ void solve(Matrix<T>& m, std::vector<T>& res, size_t variables_cnt)
 {
     if (variables_cnt == 1)
     {
-        std::vector<T> ret(m.getWidth(), 0);
-        ret[0] = *(m[0].end()--) / m[0][0];
+        res[0] = m[0].back() / m[0][0];
     }
     else
     {
-        T prod = m[0][variables_cnt - 1];
-        T gcd_of_products = prod;
-        for (int i = 1; i < variables_cnt; ++i)
+        T prod = m[variables_cnt - 1][variables_cnt - 1];
+        T gcd_res = m[variables_cnt - 1][variables_cnt - 1];
+        for (int i = 0; i < variables_cnt - 1; ++i)
         {
-            gcd_of_products = gcd(prod, m[i][variables_cnt - 1]);
-            prod = prod * m[i][variables_cnt - 1];
+            prod *= m[i][variables_cnt - 1];
+            gcd_res = gcd(gcd_res, m[i][variables_cnt - 1]);
         }
-        T lcm = prod / gcd_of_products;
-        for (auto &line : m)
+        T lcm = prod / gcd_res;
+        for (int i = 0; i < variables_cnt; ++i)
         {
-            T mul = lcm / line[variables_cnt - 1];
-            std::for_each(line.begin(), line.end(), [&mul](T& val)
-                {
-                    val = val * mul;
-                });
+            T mul = lcm / m[i][variables_cnt - 1];
+            for (int j = 0; j < m.getWidth(); ++j)
+                m[i][j] *= mul;
         }
         for (int i = 0; i < variables_cnt - 1; ++i)
-            for (int j = 0; j < variables_cnt; ++j)
-                m[i][j] = m[i][j] - m[variables_cnt - 1][j];
+            for (int j = 0; j < m.getWidth(); ++j)
+                m[i][j] -= m[variables_cnt - 1][j];
         solve(m, res, variables_cnt - 1);
-        T c = *(m[variables_cnt - 1].end()--);
-        c = c * (-1);
+        res[variables_cnt - 1] = m[variables_cnt - 1].back();
         for (int i = 0; i < variables_cnt - 1; ++i)
-            c = c - m[variables_cnt - 1][i] * res[i];
-        res[variables_cnt - 1] = c / m[variables_cnt - 1][variables_cnt - 1];
+            res[variables_cnt - 1] -= m[variables_cnt - 1][i] * res[i];
+        res[variables_cnt - 1] /= m[variables_cnt - 1][variables_cnt - 1];
     }
 }
 
 #include "fractions.h"
-//template
-//std::vector<Fractional<int>> solve(Matrix<Fractional<int>>& m);
-//template
-//void solve(Matrix<Fractional<int>>& m, std::vector<Fractional<int>>& res, size_t variables_cnt);
+template
+std::vector<Fractional<int>> solve(Matrix<Fractional<int>>& m);
+template
+void solve(Matrix<Fractional<int>>& m, std::vector<Fractional<int>>& res, size_t variables_cnt);
 template
 std::vector<int> solve(Matrix<int>& m);
 template
 void solve(Matrix<int>& m, std::vector<int>& res, size_t variables_cnt);
 
 template<typename T>
-T gcd(T a, T b);
+T gcd(T a, T b)
+{
+    while (b != T(0))
+    {
+        a %= b;
+        std::swap(a, b);
+    }
+    return a;
+}
